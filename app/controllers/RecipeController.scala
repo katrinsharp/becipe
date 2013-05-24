ddcdd3cdb96fcd35c8d468703f39127176f6a272
Katrin Shechtman <katrin.shechtman@gmail.com>
2013-05-24 02:08:26 -0400

    add recipe first draft - runtime exception upon submission

--- app/controllers/RecipeController.scala
+++ app/controllers/RecipeController.scala
@@ -38,7 +38,149 @@ case class SearchRecipesSubmit(level: Option[String] = Some(""), query: Option[S
 
 object RecipeController extends Controller with MongoController {
   
-  private def getRecipe(id: String): Option[Recipe] = {
+  val recipeForm: Form[RecipeSubmit] = Form(
+		mapping(
+			"recipe" -> mapping(	
+			"id" -> nonEmptyText,			
+			"name" -> nonEmptyText,			
+			"shortDesc" -> nonEmptyText.verifying("This field is required", (_.trim().length() > 3)),			
+			"created" -> jodaDate("yyyy-MM-dd"),			
+			"by" -> nonEmptyText,			
+			"directions" -> nonEmptyText.verifying("This field is required", (_.trim().length() > 3)),			
+			"phases" -> seq(mapping(
+					"description" -> text,
+					"ingredients" -> seq(nonEmptyText)
+					)(RecipePhase.apply)(RecipePhase.unapply)),			
+			"prepTime" -> nonEmptyText,	
+			"readyIn" -> nonEmptyText,
+			"recipeYield" -> nonEmptyText,			
+			"level" -> text.verifying("should be on of beginner, average or master", {_.matches("""^beginner|average|master""")}),			
+			"tags" -> seq(nonEmptyText),
+			"rating" -> ignored(0),
+			"photos" -> ignored(Seq[S3Photo]())
+			)(Recipe.apply)(Recipe.unapply),
+			"removed" -> seq(mapping(
+					"photoId" -> text,
+					"originKey" -> text,
+					"removedPhoto" -> boolean
+					)(photos.apply)(photos.unapply))
+			)(RecipeSubmit.apply)(RecipeSubmit.unapply))
+			
+	
+			def submitRecipe = Action {  implicit request =>
+		recipeForm.bindFromRequest.fold(
+			formWithErrors => {BadRequest(views.html.recipes.recipe_add_form(formWithErrors))},
+			value => {
+				val id = value.recipe.id match {
+							case "-1" => UniqueCode.getRandomCode
+							case v => v
+						}
+				AsyncResult {
+					
+					val newRecipe = (id != value.recipe.id)
+					
+					var photos = List[S3Photo]()
+					var originalPhotos = List[S3Photo]()
+					var isPreviewSet = false
+					
+					if(!newRecipe) {
+						val recipe = getRecipe(id)
+						recipe match {
+							case Some(r) => photos = photos ++ r.photos
+							case _ =>
+						}
+						originalPhotos = photos
+					}
+					
+					Logger.debug("photos: "+photos.length)
+					photos.foreach(f => Logger.debug(f.key))
+					Logger.debug(s"removed: "+value.s.toString)
+					
+					//val removedPhotos = photos.filter(p => {
+					//	val found = value.s.find(_.key == p.key)
+					//	found.isDefined && found.get.isRemoved 
+					//})
+					
+					photos = photos.filterNot(p => {
+						value.s.find(removed => removed.isRemoved && (removed.originKey == p.key || removed.originKey == p.metadata.originKey)).isDefined
+					})
+					
+					Logger.debug("photos: "+photos.length)
+					
+					val files = request.body.asMultipartFormData.toList
+					
+					for(i <- 0 to files.length - 1) {
+						files(i).files.map { file =>
+							if(file.ref.file.length() != 0) {
+								Logger.debug("next file")
+								val original = S3Photo.save(Image.asIs(file.ref.file), "original", "")
+								photos = photos :+ original 
+								photos = photos :+ S3Photo.save(Image.asSlider(file.ref.file), "slider", original.key)
+								if(!isPreviewSet) {
+									photos = photos :+ S3Photo.save(Image.asPreviewRecipe(file.ref.file), "preview", original.key)
+									isPreviewSet = true
+								} 
+							}
+						}
+					}
+					
+					Logger.debug(value.toString)
+					
+					photos.length match {
+						case 0 => Future(BadRequest(views.html.recipes.recipe_add_form(
+								recipeForm.fill(value).withError(FormError("recipe.photos", "Minimum one photo is required")), 
+								originalPhotos.filter(_.metadata.typeOf == "slider")
+								)))
+						case _ => {
+							val selector = QueryBuilder().query(Json.obj("id" -> value.recipe.id)).makeQueryDocument
+							val modifier = QueryBuilder().query(Json.obj(
+								"id" -> id,
+								"name" -> value.recipe.name,
+								"shortDesc" -> value.recipe.shortDesc.trim(),
+								"created" -> value.recipe.created,
+								"by" -> value.recipe.by,
+								"directions" -> value.recipe.directions.trim(),
+								"prepTime" -> value.recipe.prepTime,
+								"readyIn" -> value.recipe.readyIn,
+								"recipeYield" -> value.recipe.recipeYield,
+								"level" -> value.recipe.level,
+								"phases" -> value.recipe.phases.map(ph => RecipePhase(ph.description, ph.ingredients(0).split(",").map(_.trim()))),
+								"tags" -> value.recipe.tags(0).split(",").map(_.trim()),
+								"rating" -> value.recipe.rating,
+								"photos" -> photos)).makeQueryDocument
+							newRecipe match {
+								case false => Application.recipeCollection.update(selector, modifier).map {
+									e => {
+										Logger.debug(e.toString)
+										/*
+										 * delete associated files
+										 */
+										
+										
+										//for {
+										//	files <- Option(new File(path).listFiles)
+										//	file <- files if file.getName.endsWith(".jpg")
+										//} 
+										//file.delete()
+										
+										
+										Redirect(routes.RecipeController.get(id))
+									}
+								}
+								case true => Application.recipeCollection.insert(modifier).map {
+									e => Logger.debug(e.toString);Redirect(routes.RecipeController.get(id))
+								}
+							}
+						}	
+					}
+					
+				} 
+			}
+		)
+	}
+	
+  
+	private def getRecipe(id: String): Option[Recipe] = {
 		val qb = QueryBuilder().query(Json.obj("id" -> id))
 		val futureRecipe = Application.recipeCollection.find[JsValue](qb).toList.map(
 				_.headOption match {
@@ -70,6 +212,10 @@ object RecipeController extends Controller with MongoController {
 	  	Logger.debug(categories.toString)
 		Ok
 	}*/
+	
+	def add() = Action { implicit request =>
+		Ok(views.html.recipes.recipe_add_form(recipeForm))
+	}
   
   val searchRecipesForm: Form[SearchRecipesSubmit] = Form(
 		mapping(
