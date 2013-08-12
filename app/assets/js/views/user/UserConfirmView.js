cb0c8273d11130ac9a0ec67d45e8d6b99f7778c0
Katrin Shechtman <katrin.shechtman@gmail.com>
2013-08-12 03:43:56 -0400

    automatic logging in after successful confirmation

--- app/assets/js/views/user/UserConfirmView.js
+++ app/assets/js/views/user/UserConfirmView.js
@@ -3,10 +3,11 @@ define([
   'underscore',
   'backbone',
   'bootstrap',
+  'views/user/UserLoginView',
   'views/user/UserInputView',
   'models/user/UserConfirmModel',
   'text!templates/user/userConfirmTemplate.html'
-], function($, _, Backbone, Bootstrap, UserInputView, UserConfirmModel, userConfirmTemplate){
+], function($, _, Backbone, Bootstrap, UserLoginView, UserInputView, UserConfirmModel, userConfirmTemplate){
 
    var UserConfirmView = UserInputView.extend({
    
@@ -39,6 +40,7 @@ define([
 	confirm: function(e) {
 		this.model.save({ps: this.model.get('ps')}, {
 			success: function (model, response) {
+				UserLoginView.model.set({token: response['token'], fn: response['fn']});
 				window.location.hash = 'user-signup-complete/'+ model.get('fn');
 			},
 			error: function (model, response) {
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -21,8 +21,11 @@ import java.util.UUID
 import services.EmailMessage
 import scala.concurrent.Future
 import org.mindrot.jbcrypt.BCrypt
+import reactivemongo.core.commands.FindAndModify
+import reactivemongo.core.commands.Update
 
 case class SignupDetails(firstName: String, lastName: String, email: String)
+case class Email(email: String)
 case class Password(password: String)
 case class Login(email: String, password: String)
 
@@ -90,6 +93,53 @@ object Application extends Controller with MongoController{
 	def logout = Action { implicit request =>
 	  Ok.withNewSession
 	}
+	
+	private def  setNewTokenForSignupByEmail(email: String, token: String) = {
+		
+		val selector = QueryBuilder().query(Json.obj("email" -> email)).makeQueryDocument
+		val q = Json.obj("$set" -> Json.obj("token" -> token))
+	  
+		val modifier = QueryBuilder().query(q).makeQueryDocument	
+		
+		db.command(FindAndModify(Application.signupsCollection.name, selector, Update(modifier, true)))
+	}
+	
+	val forgotPasswordForm: Form[Email] = Form(
+		mapping(
+			"em" -> nonEmptyText
+		)(Email.apply)(Email.unapply))
+	
+	
+	def forgotPassword = Action { implicit request =>
+	  forgotPasswordForm.bindFromRequest.fold(
+			formWithErrors => {
+			  BadRequest(formWithErrors.errorsAsJson) 
+			},
+			value => {
+			  Async {
+				  val token = UUID.randomUUID().toString()	
+				  setNewTokenForSignupByEmail(value.email, token).map(
+				      f => {
+				    	  f match {
+				    	    case Some(signup) => {
+				    	    	val firstName = signup.get("firstName").get.asInstanceOf[BSONString].value
+				    	    	val confirmationLink = "http://"+request.host+"/signup/confirm/"+token
+				    	    	Application.emailService.send(new EmailMessage(
+			  										subject = "User Registration Confirmation",
+			  										recipient = value.email,
+			  										from = "info@becipe.com",
+			  										smtpConfig = Application.defaultSmtpConfig,
+			  										html = Some(s"""Hi $firstName, please click following link to reset your password: <a href="$confirmationLink">reset</a>""")
+				    	    	))
+				    	    	Ok("")
+				    	    }
+				    	    case None => BadRequest(Json.obj("em" -> "Doesn't exist"))
+				    	  }
+				      })
+				}
+			})
+	}
+	
 	val signupForm: Form[SignupDetails] = Form(
 		mapping(
 			"fn" -> nonEmptyText,
@@ -149,9 +199,9 @@ object Application extends Controller with MongoController{
 	  val q = Json.obj("$set" -> Json.obj("pass" -> BCrypt.hashpw(password, BCrypt.gensalt()), "token" -> "0"))
 	  
 	  val modifier = QueryBuilder().query(q).makeQueryDocument
-	  Application.signupsCollection.update(selector, modifier).map {
-	    e => "****"
-	  }
+	  
+	  db.command(FindAndModify(Application.signupsCollection.name, selector, Update(modifier, true)))
+	  
 	}
 	
 	val savePasswordForm: Form[Password] = Form(
@@ -167,7 +217,14 @@ object Application extends Controller with MongoController{
 			},
 			value => {
 			  Async {
-				updateSignupByToken(token, value.password).map(f => Ok(Json.obj("ps" -> f)))
+				updateSignupByToken(token, value.password).map{
+				  f => 
+				    f match {
+				      case Some(signup) => 
+				      		Ok(Json.obj("token" -> "kuku", "fn" -> signup.get("firstName").get.asInstanceOf[BSONString].value)).withSession("token" -> "kuku")
+				      case None => BadRequest(Json.obj("ps" -> "Unknown Error"))
+				    }
+				 }
 			  }
 			  
 			})
