d398b423931788273d1d7dee497897b0aa8d6022
Katrin Shechtman <katrin.shechtman@gmail.com>
2013-09-16 01:35:45 -0400

    photo error in create recipe Issue #47

--- app/assets/js/views/recipes/CreateRecipeView.js
+++ app/assets/js/views/recipes/CreateRecipeView.js
@@ -59,7 +59,7 @@ define([
 		_.each(result.errors, function(error){
 			var el = $('[data-fname=' + error.name + ']');
 			$(el).closest('.root').prev().addClass('error');
-			$(el).closest('.fileupload').append('<span class="error photo-error">' + error.message + '</span>');
+			$(el).closest('.fileupload').append('<span class="error photo-error">' + error.message + '\n[' + error.name.replace(/\_/g,'.') + ']</span>');
 		});
 		CreateRecipeView.__super__.displayError.call();//enable back the button
 		_.each(result.successes, function(success){
